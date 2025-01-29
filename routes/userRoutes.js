3b03b670905791705d52a31a7e94775212114f4f
Holly Draz <94925959+HollyDraz@users.noreply.github.com>
2025-01-29 13:25:38 -0600

    added routes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,8 +1,9 @@
 const express = require('express');
-const { getUsers, createUser } = require('../models/userModel');
+const { getUsers, createUser, deleteUser } = require('../models/userModel');
 
 const router = express.Router();
 
+// Get all users
 router.get('/', async (req, res) => {
   try {
     const users = await getUsers();
@@ -12,11 +13,23 @@ router.get('/', async (req, res) => {
   }
 });
 
+// Create a new user
 router.post('/', async (req, res) => {
   const { username, role } = req.body;
   try {
     const user = await createUser(username, role);
     res.status(201).json(user);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+// Delete a user
+router.delete('/:id', async (req, res) => {
+  const { id } = req.params;
+  try {
+    await deleteUser(id);
+    res.status(200).json({ message: 'User deleted successfully' });
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
